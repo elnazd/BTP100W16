f2e609d472bdec6d95b59f8243f038793b339501
elnazd <elnaz.delpisheh@gmail.com>
2016-01-18 10:04:53 -0500

    Create area_circle.c

---
+++ Week2/area_circle.c
@@ -0,0 +1,11 @@
+/*pi=3.14
+The area of a circle is given by the formula
+area = pi * radius * radius
+Write a program to calculate the area of a circle.
+
+step 1: define a variable to hold the area (a definition)
+define radius
+define pi
+step2: area = pi * radius * radius
+step 3: display the area to the user
+*/
