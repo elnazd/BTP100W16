6b816d4ee2e3290ebbef866ab3f1349175605b2a
elnazd <elnaz.delpisheh@gmail.com>
2016-01-18 10:02:58 -0500

    Create dataTypes.c

---
+++ Week2/dataTypes.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+	//Define a student's id:
+	int student_id;
+	student_id = 3456789;//assignment is done from right to left
+	3456789 = student_id;//WRONG
+	
+	
+	//Define a person's age
+	int age;
+	age = 20;
+	
+	//Define Canada's population
+	double population;
+	population=30000000;
+	
+	//Define PI
+	float pi = 3.14;
+	
+	//Define a letter of alphabet
+	char letter = 'a';
+	
+	/*
+	Define:
+	Books on loan 
+	Cash in a cash register
+	Humidity
+
+	*/
+	
+	//	Books on loan 
+	int books_loan;
+	//	Cash in a cash register
+	double cash;
+	//Humidity
+	float humidity;
+
+
+	return 0;
+}
