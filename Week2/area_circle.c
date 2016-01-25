7431c85b8211a4d46be01b32ca343fe0615c9fa1
elnazd <elnaz.delpisheh@gmail.com>
2016-01-24 23:29:18 -0500

    Update area_circle.c

--- Week2/area_circle.c
+++ Week2/area_circle.c
@@ -9,3 +9,20 @@ define pi
 step2: area = pi * radius * radius
 step 3: display the area to the user
 */
+#include<stdio.h>
+#define PI 3.14
+int main()
+{
+  float radius,area;
+  //Prompting the user
+  printf("Please enter the radius:");
+  scanf("%f",&radius);
+  
+  //Calculating the area:
+  area = PI * radius * radius;
+  
+  //Displaying the results:
+  printf("The area of the circle is: %.2f\n",area);
+  return 0;
+  
+}
